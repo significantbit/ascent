b205ea26986b6055b31a90388061b61c5ee1b484
Ante Wall <ante@significantbit.se>
2016-07-07 08:22:55 +0200

    Added Delete route test

--- spec/controllers/admin/nodes_controller_spec.rb
+++ spec/controllers/admin/nodes_controller_spec.rb
@@ -20,4 +20,12 @@ describe Ascent::Admin::NodesController, type: :controller do
     expect(assigns(:object)).to eq(root)
     expect(response).to render_template(:edit)
   end
+
+  it 'DELETE #destory' do
+    root = create :ascent_node, :root
+    delete :destroy, params: { id: root.id }
+    expect(assigns(:object)).to eq(root)
+    #TODO fix url here...
+    expect(response).to redirect_to('/admin/nodes')
+  end
 end
